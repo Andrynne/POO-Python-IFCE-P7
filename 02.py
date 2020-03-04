762093f15a4b1df6e2f07722a98eded0785be288
Andrynne <drynnez@gmail.com>
2020-03-04 15:08:43 -0300

    04/03/2020

---
+++ 02.py
@@ -0,0 +1,17 @@
+#retornar true or false
+
+"""idade = int(input("digite sua idade: "))
+pode = idade >= 18
+print(pode)
+idade = int(input("digite sua idade: "))
+pode = idade <= 18
+print(pode)
+idade = int(input("digite sua idade: "))
+pode = idade == 18
+print(pode)
+idade = int(input("digite sua idade: "))
+pode = idade != 18
+print(pode)"""
+ x= bool(23) 
+ print (x)
+
